586683282a41ece4b4040f894017ede3a25f563a
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-01-20 22:04:54 +0100

    normalize

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+// Імпорт стилів нормалізації
+import "modern-normalize";
+import App from "./components/App.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
