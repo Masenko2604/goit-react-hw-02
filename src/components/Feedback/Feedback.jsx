7067e6250ec38e1b0becf4f738607239d403fcfe
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-01-21 18:38:00 +0100

    update

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,11 +1,11 @@
-export const Feedback = ({ good, neutral, bad, totalFeedback, percent }) => {
+export const Feedback = ({ good, neutral, bad, allFeedback, educt }) => {
   return (
     <div>
       <p>good: {good}</p>
       <p>neutral: {neutral}</p>
       <p>bad: {bad}</p>
-      <p>totalFeedback: {totalFeedback}</p>
-      <p>positive: {percent}</p>
+      <p>allFeedback: {allFeedback}</p>
+      <p>positive: {educt}</p>
     </div>
   );
 };
