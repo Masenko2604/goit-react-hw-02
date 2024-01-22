be24e64ca01fcc26193b45fbc7098634fa5833c0
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-01-22 20:34:15 +0100

    Update Description.jsx

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -2,10 +2,7 @@ export const Description = () => {
   return (
     <div>
       <h1> Sip Happens Café</h1>
-      <p>
-        Please leave your feedback about our service by selecting one of the
-        options below.
-      </p>
+      <p>Please leave your feedback about our service by selecting one of the options below.</p>
     </div>
   );
 };
